6b973958a278b2f2aaa8899e69d9985748159d6e
yichang.yyf <yangyifei.yyf@antgroup.com>
2023-02-08 11:42:00 +0800

    move novic to winbar

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,8 +7,13 @@ return {
       opts.options = {
         disabled_filetypes = {
           statusline = { "dashboard", "lazy", "neo-tree" },
+          winbar = { "dashboard", "lazy", "neo-tree" },
         },
       }
+      -- remove navic from the statusline
+      local navic = table.remove(opts.sections.lualine_c)
+      -- add it to the winbar instead
+      opts.winbar = { lualine_a = { "filename" }, lualine_c = { navic } }
     end,
   },
   {
