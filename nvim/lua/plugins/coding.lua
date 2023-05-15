0c951c00a797eec3b1994ae51ff65287ee28348d
yichang.yyf <yangyifei.yyf@antgroup.com>
2023-05-15 10:05:38 +0800

    modify clang format and comment

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -3,9 +3,6 @@ return {
   {
     "numToStr/Comment.nvim",
     event = "VeryLazy",
-    opts = {
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    },
   },
   -- add nvim-surround
   {
