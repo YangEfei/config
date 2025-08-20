fd47638e3fb3c09dd1dd9cf89caed55da0c159b1
yangyifei.yyf <yangyifei96@outlook.com>
2025-08-20 17:13:56 +0800

    Remove nvim.bak

--- nvim.bak/lua/plugins/diffview.lua
+++
@@ -1,7 +0,0 @@
-local status, diffview = pcall(require, "diffview")
-if not status then
-  vim.notify("没有找到 diffview")
-  return
-end
-
-diffview.setup()
--- nvim.bak/lua/plugins/project.lua
+++
@@ -1,7 +0,0 @@
-local status, project = pcall(require, "project_nvim")
-if not status then
-  vim.notify("没有找到 project")
-  return
-end
-
-project.setup() 
